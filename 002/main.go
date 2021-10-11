d1ab672f517cd07f24e6240f034b88fe2af19e4f
Grant Forsythe <grantwforsythe@gmail.com>
2021-10-11 12:44:42 -0400

    Rewrite problems in Go

---
+++ 002/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const n int = 4000000
+
+func fib() int {
+	/*
+	   Description:    Compute the Fibonacci Sequence.
+	   Parameters: 	none
+	   Return:	        the sum of the even-valued terms in the Fibonacci Sequence whose values
+	                   don't exceed 4 million
+	*/
+	result := 0
+	// starting values
+	a := 0
+	b := 1
+	for a+b < n {
+		temp := a + b
+		if temp%2 == 0 {
+			result += temp
+		}
+		a = b
+		b = temp
+	}
+	return result
+}
+
+func main() {
+	fmt.Println("Answer: ", fib())
+}
