a4b27171b987d3d9db0be7dee1d51b55e8454534
Grant Forsythe <grantwforsythe@gmail.com>
2021-10-14 10:00:01 -0400

    Formatting

--- 001/main.go
+++ 001/main.go
@@ -6,25 +6,25 @@ import "fmt"
 const n int = 1000
 
 func main() {
-	fmt.Println("Answer: ", multiples_3or5())
+    fmt.Println("Answer: ", multiples_3or5())
 }
 
 func multiples_3or5() int {
-	/*
-	   Description:    Finds the sum of all the multiples of 3 and 5 less than n.
-	   Parameters:     none
-	   Return:         the sum of all natural numbers less than n that are divisible by 3 or 5
-	   Example of Usage:
-	   >> const n int = 10
-	   >> result := multiples_3or5()
-	   ans =
-	       23
-	*/
-	result := 0
-	for num := 1; num < n; num++ {
-		if num%3 == 0 || num%5 == 0 {
-			result += num
-		}
-	}
-	return result
+    /*
+       Description:    Finds the sum of all the multiples of 3 and 5 less than n.
+       Parameters:     none
+       Return:         the sum of all natural numbers less than n that are divisible by 3 or 5
+       Example of Usage:
+       >> const n int = 10
+       >> result := multiples_3or5()
+       ans =
+           23
+    */
+    result := 0
+    for num := 1; num < n; num++ {
+        if num%3 == 0 || num%5 == 0 {
+            result += num
+        }
+    }
+    return result
 }
